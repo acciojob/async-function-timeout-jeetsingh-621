27657ed16463314a2b1a439cabc4845ccd9c3a49
JITENDER <138762808+jeetsingh-621@users.noreply.github.com>
2024-12-18 10:37:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+
+function delay(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function handlesubmision() {
+	 const text = document.getElementById('text').value;
+  const delayValue = parseInt(document.getElementById('delay').value);
+  const outputDiv = document.getElementById('output');
+
+
+	 if (!text) {
+    outputDiv.textContent = "Please enter some text.";
+    return;
+  }
+	 if (isNaN(delayValue) || delayValue < 0) {
+    outputDiv.textContent = "Please enter a valid delay (positive number).";
+    return;
+  }
+
+	  outputDiv.textContent = "Waiting...";
+
+	  await delay(delayValue);
+	  outputDiv.textContent = text;
+}
+document.getElementById('btn').addEventListener('click', handlesubmision);
